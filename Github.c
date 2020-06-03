188f49df84fa18d39b29304d12d12137cbba5bdd
zeroseo74 <ys070400@daum.net>
2020-06-03 20:30:26 +0900

    나머지연산 추가 c언어 고침

--- Github.c
+++ Github.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main(){
 	printf("Hello World\n");
-	return 0;
 
 	int a, b;
 	scanf("%d %d",&a,&b);
@@ -10,4 +9,6 @@ int main(){
 	printf("두 수의 곱은 %d 입니다.\n",a*b);
 	printf("두 수의 나눗셈은 %d 입니다.\n",a/b);
 	printf("두 수의 나머지는 %d 입니다.\n",a%b);
+	
+	return 0;
 }
