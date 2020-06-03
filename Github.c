5e253d7f664e622573bf2c19f5616148d706dee3
zeroseo74 <ys070400@daum.net>
2020-06-03 20:26:22 +0900

    사칙연산 추가  c언어

--- Github.c
+++ Github.c
@@ -2,4 +2,11 @@
 int main(){
 	printf("Hello World\n");
 	return 0;
+
+	int a, b;
+	scanf("%d %d",&a,&b);
+	printf("두 수의 합은 %d 입니다.\n",a+b);
+	printf("두 수의 차는 %d 입니다.\n",a-b);
+	printf("두 수의 곱은 %d 입니다.\n",a*b);
+	printf("두 수의 나눗셈은 %d 입니다.\n",a/b);
 }
